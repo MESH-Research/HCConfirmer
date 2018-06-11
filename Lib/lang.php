c81a712dde5ada85f49e036bb8f654ff53146772
root <root@registry.hcommons-dev.org>
2018-06-11 19:16:27 +0000

    Text change for AUP.

--- Lib/lang.php
+++ Lib/lang.php
@@ -49,5 +49,14 @@ $cm_h_c_confirmer_texts['en_US'] = array(
                 <li> Register for Humanities Commons (you can always contact us if you believe you should have access to %1$s Commons)</li>
         </ul>
 </p>',
-  'op.hcc.email.na.up' => '<p>Sample text</p>'
+'op.hcc.email.na.up' => '<p>This email address is not currently associated with an AUPresses member staff record in our system.
+ 
+UP Commons is only available to staff at AUPresses members. If your press is a member (check here <a href="http://www.aupresses.org/aaup-members/membership-list">http://www.aupresses.org/aaup-members/membership-list</a> to confirm), contact us at commons@aupresses.org and provide the following information:
+<ul>
+<li>First and Last Name</li>
+<li>Job Title</li>
+<li>Press name</li>
+<li>Email you plan to register with at UP Commons (the email address MUST be affiliated with your institution).</li>
+</ul>
+If your press is not a member, request information here: <a href="http://www.aupresses.org/aaup-members/becoming-a-member/membership-interest-form">http://www.aupresses.org/aaup-members/becoming-a-member/membership-interest-form</a>.</p>'
 );
